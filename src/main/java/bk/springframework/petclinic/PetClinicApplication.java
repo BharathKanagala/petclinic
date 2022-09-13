e96868ae864a961744cc8529dcdde96d3e44142d
bharath kanagala <bharathkanagala19@gmail.com>
2022-09-13 18:59:12 +0530

    refactor main class name

--- src/main/java/bk/springframework/petclinic/PetClinicApplication.java
+++ src/main/java/bk/springframework/petclinic/PetClinicApplication.java
@@ -2,13 +2,14 @@ package bk.springframework.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class PetclinicApplication {
+public class PetClinicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PetclinicApplication.class, args);
+		SpringApplication.run(PetClinicApplication.class, args);
 	}
 
 }
+
+
